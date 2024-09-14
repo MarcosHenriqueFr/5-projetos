9d157143b0143c759ca3b81e1bceee954be7fba2
Marcos Henrique <161951682+MarcosHenriqueFr@users.noreply.github.com>
2024-09-14 20:40:41 -0300

    atualizações

---
+++ 03-Login/js/index.js
@@ -0,0 +1,45 @@
+
+const form = document.querySelector("form");
+const usuario = document.getElementById("usuario");
+const senha = document.getElementById("senha");
+const divGeral = document.getElementById("container-geral");
+
+//Só vai acontecer depois de pressionar o botão
+form.addEventListener("submit", event => {
+
+    event.preventDefault();
+    
+    let segurarSenha = String(senha.value);
+    let segurarUsuario = String(usuario.value);
+
+    checarEmail(segurarUsuario);
+    checarSenha(segurarSenha);
+});
+
+function checarEmail(usuarioinput){
+    //meu regex
+    const emailval = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    const aprovado = emailval.test(usuarioinput);
+
+    usuario.parentElement.classList.remove("verde", "vermelho");
+    if(aprovado){
+        usuario.parentElement.classList.add("verde");
+    } else {
+        usuario.parentElement.classList.add("vermelho");
+    }
+}
+
+function checarSenha(senhainput){
+    //meu regex
+    const senhaval = /()/;
+
+    const aprovado = senha.test(senhainput);
+
+    usuario.parentElement.classList.remove("verde", "vermelho");
+    if(aprovado){
+        senha.parentElement.classList.add("verde");
+    } else {
+        senha.parentElement.classList.add("vermelho");
+    }
+}
